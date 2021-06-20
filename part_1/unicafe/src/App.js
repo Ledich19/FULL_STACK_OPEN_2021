93513cdf1367f1cf67249e23a94c5c639e927bea
Aleksandr <ledich19@gmail.com>
2021-06-20 10:06:42 +0300

    1.6: unicafe step11.7: unicafe step2

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react'
 
 const Header = ({title}) => <h2>{title}</h2>;
-const Button = (props) => {
-  return <button onClick={props.handleClick} >{props.text}</button>
+const Button = ({handleClick,text}) => {
+  return <button onClick={handleClick} >{text}</button>
   };
-const Statistics = ({good,neutral,bad}) => {
-  return <>
-    <div>{good}</div>
-    <div>{neutral}</div>
-    <div>{bad}</div>
-  </>
-}
-
-function setValue() {
-
+const Display = ({text,parameter}) => {
+  return <div>{text}: {parameter}</div>
 }
 
 const App = () => {
@@ -21,15 +13,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad;
+  const average = (good - bad)/all;
+  const positive = good/all*100
 
   return (
     <div>
       <Header title={'give feedback'} />
-      <Button text={'good'} handleClick={()=> setGood(good+1)} />
-      <Button text={'neutral'} handleClick={()=> setNeutral(neutral+1)} />
-      <Button text={'bad'} handleClick={()=> setBad(bad+1)} />
+
+      <Button text={'good'} handleClick={()=> setGood(good + 1)} />
+      <Button text={'neutral'} handleClick={()=> setNeutral(neutral + 1)} />
+      <Button text={'bad'} handleClick={()=> setBad(bad + 1)} />
+
       <Header title={'statictics'} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Display  text={'good'} parameter={good} />
+      <Display  text={'neutral'} parameter={neutral} />
+      <Display  text={'bad'} parameter={bad} />
+      <Display  text={'all'} parameter={all} />
+      <Display  text={'average'} parameter={average} />
+      <Display  text={'positive'} parameter={positive} />
     </div>
   )
 }
